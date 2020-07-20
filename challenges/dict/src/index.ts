9ebcf9550057a20aceb634ed9aceb37c252c2e8c
Moses A. Samuel <mosamuel98@gmail.com>
2020-07-19 20:31:24 -0700

    complete the generics exercise

--- challenges/dict/src/index.ts
+++ challenges/dict/src/index.ts
@@ -1,7 +1,45 @@
-export type Dict = {};
+import { countReset } from "console";
+
+export type Dict<A> = {
+	[key: string]: A;
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<A, B>(
+	mapperFn: (valueToBeMapped: A, index: number, dict: Dict<A>) => B,
+	dict: Dict<A>
+): Dict<B> {
+	const mappedDict: Dict<B> = {};
+	let counter = 0;
+
+	for (let [key, value] of Object.entries(dict)) {
+		mappedDict[key] = mapperFn(value, counter, dict);
+		counter++;
+	}
+	return mappedDict;
+}
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+export function reduceDict<A, B>(
+	reducerFn: (
+		accumulator: B | undefined,
+		current: A,
+		index: number,
+		dict: Dict<A>
+	) => B,
+	dict: Dict<A>
+): B | undefined {
+	let accumulator;
+	let counter = 0;
+	for (let value of Object.values(dict)) {
+		accumulator = reducerFn(accumulator, value, counter, dict);
+		counter++;
+	}
+	return accumulator;
+}
+
+let obj = { a: 1, b: 2, c: 3 };
+console.log(mapDict(x => x * 2, obj)); // {a:2, b: 4, c: 6}
+console.log(
+	reduceDict((acc, cur) => (acc != undefined ? acc + cur : 0 + cur), obj)
+); // 6
